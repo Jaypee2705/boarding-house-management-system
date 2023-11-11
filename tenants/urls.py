f79595dc734ad998e744635691fa39dcaa8546ac
John Mathew D. Diño <dinomathew@sorsu.edu.ph>
2023-11-12 00:44:40 +0800

    init add tenants

--- tenants/urls.py
+++ tenants/urls.py
@@ -4,4 +4,5 @@ from tenants import views
 
 urlpatterns = [
     path('tenants_profile/', views.tenants_profile, name='tenants_profile'),
+    path('tenants_profile/add/', views.add_tenants, name='add_tenants'),
 ]
--- tenants/views.py
+++ tenants/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def tenants_profile(request):
     return render(request, 'tenants/tenants_profile.html')
+
+
+def add_tenants(request):
+    return render(request, 'tenants/add_tenants.html')
