16766f6d0b28695098507a28847ba4ddd03e7ef0
John Mathew D. Diño <dinomathew@sorsu.edu.ph>
2023-11-12 01:05:28 +0800

    init payments

--- payments/urls.py
+++ payments/urls.py
@@ -4,4 +4,5 @@ from payments import views
 
 urlpatterns = [
     path('utility-bill/', views.utility_bill, name='utility-bill'),
+    path('payments/', views.payments, name='payments'),
     ]
--- payments/views.py
+++ payments/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def utility_bill(request):
     return render(request, 'payments/utility-bill.html')
+
+
+def payments(request):
+    return render(request, 'payments/payments.html')
