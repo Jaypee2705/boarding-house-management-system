cd96b6132d13879e9be4fe945056862f49ca45d9
John Mathew D. Diño <dinomathew@sorsu.edu.ph>
2023-11-17 01:38:47 +0800

    removed custom user

--- authentication/models.py
+++ authentication/models.py
@@ -2,6 +2,3 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
-class Custom_User(AbstractUser):
-    address = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=20)
