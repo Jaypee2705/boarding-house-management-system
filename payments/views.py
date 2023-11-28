bf774517f40bb46eb9459155466343041e6293eb
John Mathew D. Diño <dinomathew@sorsu.edu.ph>
2023-11-29 01:51:40 +0800

    collectibles

--- payments/views.py
+++ payments/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from boardinghouse.models import Room
 from payments.forms import BillsForm, PaymentsForm
 from payments.models import Bills, Payments
+from tenants.models import Tenant
 
 
 # Create your views here.
@@ -59,4 +60,69 @@ def income(request):
 
 
 def collectibles(request):
-    return render(request, 'payments/collectibles.html')
+    tenants = Tenant.objects.filter(room__isnull=False)
+
+    """
+    [
+        {
+            tenant: 'John Doe',
+            room: 'Room 1',
+            monthly_due: 1000,
+            previous_balance: 1000,
+            total_due: 2000,
+            amount_paid: 1000,
+            current_balance: 1000,
+        },
+        {
+            tenent: 'Jane Doe',
+            room: 'Room 2',
+            monthly_due: 1000,
+            previous_balance: 1000,
+            total_due: 2000,
+            amount_paid: 1000,
+            current_balance: 1000,
+        }
+    
+    
+    ]
+    
+    """
+    collectibles_lists = []
+
+    for tenant in tenants:
+
+        total_due = 0
+        try:
+            total_due = float(tenant.previous_balance) + float(Bills.objects.get(room=tenant.room).rate)
+        except:
+            pass
+
+        amount_paid = 0
+        try:
+            for payment in Payments.objects.filter(tenant=tenant):
+                amount_paid += float(payment.amount)
+        except:
+            pass
+
+        current_balance = 0
+        try:
+            current_balance = total_due - amount_paid
+        except:
+            pass
+        collectibles_lists.append({
+            'tenant': tenant.name.get_full_name(),
+            'room': tenant.room.name,
+            'monthly_due': Bills.objects.get(room=tenant.room).rate,
+            'previous_balance': tenant.previous_balance,
+            'total_due': total_due,
+            'amount_paid': amount_paid,
+            'current_balance': current_balance,
+        })
+
+
+
+
+    return render(request, 'payments/collectibles.html',{
+        'tenants': tenants,
+        'collectibles_lists': collectibles_lists,
+    })
--- tenants/models.py
+++ tenants/models.py
@@ -6,7 +6,8 @@ class Tenant(models.Model):
     owner = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='owner', null=True, blank=True)
     room = models.ForeignKey('boardinghouse.Room', on_delete=models.CASCADE, null=True, blank=True)
     date_start = models.DateField()
-
+    current_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    previous_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     def __str__(self):
         return self.name.get_full_name()
 
