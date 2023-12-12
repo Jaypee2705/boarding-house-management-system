efc1387e326d63a82b460103b2b2d100b21673d6
John Mathew D. Diño <dinomathew@sorsu.edu.ph>
2023-12-13 02:32:56 +0800

    init owner cellphone

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from authentication.models import Cellphone_number
+
 # Register your models here.
+admin.site.register(Cellphone_number)
--- authentication/models.py
+++ authentication/models.py
@@ -1,4 +1,11 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, User
 from django.db import models
 
 # Create your models here.
+
+class Cellphone_number (models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cellphone_number = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.cellphone_number
